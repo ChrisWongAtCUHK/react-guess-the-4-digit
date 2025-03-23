22b4785b97573251f8aa7edd54e992ca650960c6
Chris Wong <chris.wong.ksl@gmail.com>
2025-03-23 23:00:25 +0800

    Start game

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,136 @@
+import {
+  ChangeEvent,
+  FormEvent,
+  FormEventHandler,
+  useEffect,
+  useRef,
+  useState,
+} from 'react'
 import './App.css'
 import CircleInfo from './components/icons/CircleInfo'
+import PaperPlaneIcon from './components/icons/PaperPlaneIcon'
+import RotateLeftIcon from './components/icons/RotateLeftIcon'
 
+interface Attempt {
+  guess: string
+  cows: number
+  bulls: number
+}
 function App() {
+  const attempts = useRef<Attempt[]>([])
+  const secretNumber = useRef('')
+  const [guess, setGuess] = useState('')
+  const [message, setMessage] = useState({
+    show: false,
+    text: '',
+    variant: '',
+  })
+
+  function isMobile() {
+    return window.innerWidth <= 768
+  }
+
+  function generateSecretNumber() {
+    const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    let secret = ''
+    for (let i = 0; i < 4; i++) {
+      const randomIndex = Math.floor(Math.random() * numbers.length)
+      secret += numbers[randomIndex]
+      numbers.splice(randomIndex, 1)
+    }
+    return secret
+  }
+
+  function hasDuplicates(value: string) {
+    const digits = value.split('')
+    return digits.some(
+      (digit: string, index: number) => digits.indexOf(digit) !== index
+    )
+  }
+
+  function validateInput(e: ChangeEvent<HTMLInputElement>) {
+    const inputElement = e.target as HTMLInputElement
+    const currentValue = inputElement.value
+    const validValue = currentValue.replace(/[^1-9]/g, '')
+
+    if (currentValue !== validValue) {
+      inputElement.value = validValue
+      inputElement.setCustomValidity(
+        'Please enter a 4-digit number with digits from 1 to 9 (inclusive)'
+      )
+    } else if (hasDuplicates(currentValue)) {
+      inputElement.setCustomValidity('No duplicate digits allowed')
+    } else {
+      inputElement.setCustomValidity('')
+      setGuess(() => inputElement.value)
+    }
+  }
+
+  function calculateBullsAndCows(guess: string) {
+    let bulls = 0
+    let cows = 0
+
+    for (let i = 0; i < guess.length; i++) {
+      if (guess[i] === secretNumber.current[i]) {
+        bulls++
+      }
+      if (secretNumber.current.includes(guess[i])) {
+        cows++
+      }
+    }
+
+    return { bulls, cows }
+  }
+
+  function submitGuess(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    if (attempts.current.length < 10) {
+      const { bulls, cows } = calculateBullsAndCows(guess)
+      attempts.current.push({
+        guess: guess,
+        cows,
+        bulls,
+      })
+
+      if (bulls === 4) {
+        setMessage(() => {
+          return {
+            show: true,
+            text: `You won! The secret number is ${secretNumber}.`,
+            variant: 'success',
+          }
+        })
+      } else if (attempts.current.length === 10) {
+        setMessage(() => {
+          return {
+            show: true,
+            text: `Game over. The secret number was ${secretNumber}.`,
+            variant: 'danger',
+          }
+        })
+      }
+    }
+
+    setGuess(() => '')
+  }
+
+  function resetGame() {
+    attempts.current = []
+    secretNumber.current = generateSecretNumber()
+    setMessage(() => {
+      return {
+        show: false,
+        text: '',
+        variant: '',
+      }
+    })
+    window.scrollTo({ top: 0, behavior: 'smooth' })
+  }
+
+  useEffect(() => {
+    secretNumber.current = generateSecretNumber()
+  }, [])
+
   return (
     <div className='container-sm'>
       <div className='row justify-content-md-center'>
@@ -30,9 +159,94 @@ function App() {
                     </span>
                   </span>
                 </th>
+                <th scope='col' className='corrects-column'>
+                  <span className='d-flex align-items-center popper'>
+                    <span className='me-1'>Corrects</span>
+                    <CircleInfo />
+                    <span className='poppertext'>
+                      Number of digits in the guess that <br />
+                      are in the correct position.
+                    </span>
+                  </span>
+                </th>
               </tr>
             </thead>
+            <tbody>
+              {Array(10).map((_, index) => {
+                return (
+                  <tr key={index} className='table-row'>
+                    <td>
+                      {attempts.current[index]?.guess ? (
+                        <span className='fade-in fade-out'>
+                          {attempts.current[index]?.guess}
+                        </span>
+                      ) : null}
+                    </td>
+                    <td>
+                      {attempts.current[index]?.cows !== undefined ? (
+                        <span className='fade-in fade-out'>
+                          {attempts.current[index]?.cows}
+                        </span>
+                      ) : null}
+                    </td>
+                    <td>
+                      {attempts.current[index]?.bulls !== undefined ? (
+                        <span className='fade-in fade-out'>
+                          {attempts.current[index]?.bulls}
+                        </span>
+                      ) : null}
+                    </td>
+                  </tr>
+                )
+              })}
+            </tbody>
           </table>
+          {message.show ? null : (
+            <form onSubmit={submitGuess} className='form-inline d-flex'>
+              <div className='input-group'>
+                <input
+                  value={guess}
+                  type='text'
+                  pattern='^(?!.*(.).*\1)[1-9]{1,4}$'
+                  maxLength={4}
+                  placeholder='Enter your guess'
+                  required
+                  className='form-control'
+                  disabled={attempts.current.length === 10 || message.show}
+                  readOnly={isMobile()}
+                  onChange={validateInput}
+                />
+                <button
+                  disabled={guess.length < 4}
+                  type='submit'
+                  className='btn btn-primary ml-2'
+                >
+                  <PaperPlaneIcon />
+                </button>
+              </div>
+            </form>
+          )}
+          {message.show ? (
+            <div
+              className={['alert mt-3', `alert-${message.variant}`].join(' ')}
+            >
+              {message.text}
+              <button
+                onClick={resetGame}
+                className='btn btn-sm btn-outline-secondary ml-2'
+              ></button>
+            </div>
+          ) : (
+            <div className='text-center my-4'>
+              <button
+                onClick={resetGame}
+                className='btn btn-sm btn-outline-secondary ml-2'
+              >
+                <RotateLeftIcon className='me-1' />
+                Reset
+              </button>
+            </div>
+          )}
         </div>
       </div>
     </div>
